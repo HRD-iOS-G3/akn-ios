b8b4cb25917d497177a22ccad5a7c31159365bf3
Ponnreay <ponnreay@ponnreays-macbook-pro.local>
2016-01-01 20:06:40 +0700

    Initial Commit

---
+++ AKN/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AKN
+//
+//  Created by Ponnreay on 1/1/16.
+//  Copyright © 2016 kshrd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AKN/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  AKN
+//
+//  Created by Ponnreay on 1/1/16.
+//  Copyright © 2016 kshrd. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+	[super didReceiveMemoryWarning];
+	// Dispose of any resources that can be recreated.
+}
+
+@end
